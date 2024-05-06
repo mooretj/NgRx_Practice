9e7574b6057af8b5b35095a80690e8ec795c50a6
TJ Moore <tmoore@avifoodsystems.com>
2024-05-06 15:12:42 -0400

    Login complete and added ngrx store functionality w/o persistence.

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,35 @@
+// import { isDevMode } from '@angular/core';
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector, createReducer,
+  createSelector,
+  MetaReducer, on
+} from '@ngrx/store';
+import {User} from "../model/user.model";
+import {AuthActions} from "../action-types";
+
+export const authFeatureKey = 'auth';
+
+export interface AuthState {
+  user: User
+}
+
+export const initialState: AuthState = {
+  user: undefined
+};
+
+
+export const authReducer = createReducer(
+  initialState,
+  on(AuthActions.login, (state, action) => {
+    return {
+      user: action.user
+    }
+  }),
+  on(AuthActions.logout, (state, action) => {
+    return {
+      user: undefined
+    }
+  })
+)
---
+++ src/app/reducers/index.ts
@@ -0,0 +1,20 @@
+import { isDevMode } from '@angular/core';
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer
+} from '@ngrx/store';
+import {environment} from "../../environments/environment";
+
+export interface AppState {
+
+}
+
+export const reducers: ActionReducerMap<AppState> = {
+
+};
+
+
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
